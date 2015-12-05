364d1d6318b75188c8c760984de47714e8e692e1
Koen Klaren <koen.klaren@gmail.com>
2015-12-05 01:57:39 +0100

    Implement VFS

--- src/omni-local.js
+++ src/omni-local.js
@@ -1,9 +1,11 @@
 import fs from 'fs';
-import path from 'path';
+import mpath from 'path';
 import mime from 'mime-types';
 import Promise from 'bluebird';
 
 import { OmniBase, Stats } from 'omni';
+
+Promise.promisifyAll(fs);
 
 export default class OmniLocal extends OmniBase {
 
@@ -11,6 +13,40 @@ export default class OmniLocal extends OmniBase {
     super();
 
     this._root = root;
+  }
+
+  readdir(path) {
+    let fullPath = this._getFullPath(path);
+
+    return fs.readdirAsync(fullPath);
+  }
+
+  readfile(path) {
+    let fullPath = this._getFullPath(path);
+
+    return new Promise((resolve) => {
+      resolve(fs.createReadStream(fullPath));
+    });
+  }
+
+  stat(path) {
+    let fullPath = this._getFullPath(path);
+
+    return fs.statAsync(fullPath)
+      .then((stats) => new Stats({
+        type: this._getTypeFromStats(stats),
+        mime: mime.lookup(path)
+      }));
+  }
+
+  _getFullPath(path) {
+    return mpath.join(this._root, path);
+  }
+
+  _getTypeFromStats(stats) {
+    if (stats.isFile()) return 'file';
+    if (stats.isDirectory()) return 'directory';
+    throw new Error('unknown type');
   }
 
 }
